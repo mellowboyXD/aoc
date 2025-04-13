95219a9be24da0082094d01397ac672b9abae24f
mellowboyXD <chamerallyishfaq@gmail.com>
2025-04-13 22:28:53 +0400

    read token one by one

--- src/day3/main.c
+++ src/day3/main.c
@@ -1,6 +1,24 @@
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define NORTH '^'
+#define SOUTH 'v'
+#define EAST '>'
+#define WEST '<'
 
 int main(void) {
-	printf("Hello world\n");
-	return 0;
+  FILE *fptr = fopen("src/day3/input", "r");
+  if (fptr == NULL) {
+    fputs("Unable to open file.\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  int token;
+  while ((token = fgetc(fptr)) != EOF) {
+    fprintf(stdout, "%c\n", token);
+  }
+
+  fclose(fptr);
+  exit(EXIT_SUCCESS);
 }
